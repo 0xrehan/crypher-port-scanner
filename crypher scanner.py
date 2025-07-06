859225b4bd2a12831951d77b1a0190773169863a
Rehan ali khan <rrehanalikhan666@gmail.com>
2025-07-06 17:26:28 +0530

    Create crypher scanner.py

---
+++ crypher scanner.py
@@ -0,0 +1,12 @@
+# Crypher Port Scanner 🔍
+
+A fast and lightweight port scanner built with Python for ethical hackers and cybersecurity learners.
+
+## ⚙️ Features
+- Custom IP and Port scanning
+- Multithreaded speed boost
+- CLI-based minimal interface
+
+## 🚀 How to Use
+```bash
+python3 crypher_scanner.py --ip 192.168.1.1 --ports 20-1000
